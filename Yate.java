f8ea1a3a93dcf350bf2c92944495125057fafc44
Jose Luis Fernandez <fernandezjl25@gmail.com>
2017-04-28 10:26:00 +0200

    comentarios en clases barcos y persona

--- Yate.java
+++ Yate.java
@@ -1,15 +1,33 @@
 
+/**
+ * Clase con los datos específicos de un yate.
+ * @author Jose Luis Fernández Cañón
+ * @version Version: 28/04/2016
+ */
 public class Yate extends EmbarcacionAMotor
 {
     
     private int numCamarotes;
     
+    /**
+     * Constructor que inicializa los atributos con datos pasados por parámetros.
+     * @param mtricula Matrícula del velero.
+     * @param eslora Medida del largo del velero.
+     * @param ano Año de fabricación del velero.
+     * @param propietario Dueño del velero.
+     * @param potencia Potencia del yate.
+     * @param nCamarotes Número de camarotes del yate.
+     */
     public Yate(String matricula, double eslora, int ano, Persona propietario, int potencia, int nCamarotes)
     {
         super(matricula, eslora, ano, propietario, potencia);
         numCamarotes = nCamarotes;
     }
     
+    /**
+     * Muestra la información del objeto.
+     * @return Cadena con la información sobre el objeto.
+     */
     public String toString()
     {
         String textoADevolver = "";
@@ -18,6 +36,10 @@ public class Yate extends EmbarcacionAMotor
         return textoADevolver;
     }
     
+    /**
+     * Devuelve el coeficiente de bernua del yate.
+     * @return Potencia del yate + el número de camarotes.
+     */
     public int getCoeficienteBernua()
     {
         return super.getCoeficienteBernua() + numCamarotes;
